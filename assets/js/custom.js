ac5db5adfee07cbfb61b7e4d9705c943ac90c046
vedangj044 <vedangj044@gmail.com>
2021-03-30 22:57:04 +0530

    Added random quote

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,22 @@
 // Scroll Top
+quoteservice()
+setInterval(function() {
+  quoteservice();
+}, 20000);
+
+function quoteservice () {
+  fetch("https://api.quotable.io/random?maxLength=65")
+  .then(res => res.json())
+  .then(res => { 
+    $("#quotes").fadeTo(5000, 1);
+    $("#quotes").text("\"" + res['content'] + "\" - " + res['author']); 
+    setTimeout(function() {
+      $("#quotes").fadeTo(5000, 0);
+    }, 10000);
+  })
+  .catch(err => console.log(err))
+}
+
 $('.top').click(function() {
   $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 });
